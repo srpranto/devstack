0d46123859ccf3d021ddd95067f992fedfa1a355
srpranto <shahilrahmanpranto@gmail.com>
2026-09-12 17:07:59 +0600

    refine skeleton card to 1:1 match TechCard

--- src/components/TechCardSkeleton.tsx
+++ src/components/TechCardSkeleton.tsx
@@ -8,24 +8,24 @@ const TechCardSkeleton = () => {
       <div>
         <div className="flex items-start justify-between">
           <div className="skeleton w-10 h-10 rounded-xl shrink-0" />
-          <div className="skeleton h-5 w-16 rounded-md" />
+          <div className="skeleton h-5 w-16 rounded-full" />
         </div>
 
         <div className="skeleton h-5 w-28 rounded-md mt-4" />
 
-        <div className="space-y-1.5 mt-2 min-h-9">
+        <div className="space-y-1.5 mt-1.5 min-h-9">
           <div className="skeleton h-3.5 w-full rounded" />
           <div className="skeleton h-3.5 w-4/5 rounded" />
         </div>
 
         <div className="flex items-center justify-between mt-4 pt-3 border-t border-slate-100">
-          <div className="skeleton h-4 w-16 rounded-full" />
+          <div className="skeleton h-4.5 w-16 rounded-full" />
           <div className="skeleton h-4 w-14 rounded" />
           <div className="skeleton h-4 w-10 rounded" />
         </div>
       </div>
 
-      <div className="skeleton h-9.5 w-full rounded-xl mt-4" />
+      <div className="skeleton h-9 w-full rounded-xl mt-4" />
     </div>
   );
 };
