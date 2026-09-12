129b9c9481e0fb164af978053d644c13c4390fe8
srpranto <shahilrahmanpranto@gmail.com>
2026-09-12 16:44:24 +0600

    minor fix

--- src/components/TechCard.tsx
+++ src/components/TechCard.tsx
@@ -38,7 +38,6 @@ const TechCard = ({ tech, onAdd, isAdded }: TechCardProps) => {
 
       <button
         type="button"
-        disabled={isAdded}
         aria-disabled={isAdded}
         onClick={() => onAdd(tech)}
         className={`mt-4 w-full py-2.5 px-4 rounded-xl text-xs font-semibold transition-all ${
